e72c9983ff005debd1933ccbdac48af13be3a8a4
Phillip Schanely <pschanely@gmail.com>
2023-12-03 12:56:44 -0500

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# python-tool-competition-2024-crosshair
-CrossHair harness for SBFT 2024's test case generation competition
+# Python Tool Competition Implementation Using crosshair
+
+Uses the python-tool-competition-2024 to generate tests using the
+CrossHair.
+
+For more information see
+<https://github.com/ThunderKey/python-tool-competition-2024/>.
+
+## Installation
+
+* Install [poetry](https://python-poetry.org/)
+* Run `poetry install`
+
+## Development
+
+The entry point called by `python-tool-competition-2024` is the `build_test`
+method in `python_tool_competition_2024_crosshair/generator.py`.
+
+## Calculating Metrics
+
+Run `poetry run python-tool-competition-2024 run crosshair`.
+
+With `poetry run python-tool-competition-2024 run -h` you can find out what
+generators were detected.
---
+++ python_tool_competition_2024_crosshair/generator.py
@@ -0,0 +1,58 @@
+"""A test generator using CrossHair."""
+
+from io import StringIO
+import traceback
+from python_tool_competition_2024.generation_results import (
+    FailureReason,
+    TestGenerationFailure,
+    TestGenerationResult,
+    TestGenerationSuccess,
+)
+from python_tool_competition_2024.generators import FileInfo, TestGenerator
+from crosshair.main import cover, command_line_parser
+from crosshair.options import DEFAULT_OPTIONS, AnalysisOptionSet
+
+
+class CrosshairTestGenerator(TestGenerator):
+    """A test generator using CrossHair."""
+
+    def build_test(self, target_file_info: FileInfo) -> TestGenerationResult:
+        """
+        Genereate a test for the specific target file.
+
+        Args:
+            target_file: The `FileInfo` of the file to generate a test for.
+
+        Returns:
+            Either a `TestGenerationSuccess` if it was successful, or a
+            `TestGenerationFailure` otherwise.
+        """
+
+        retcode = 0
+        try:
+            stdout, stderr = StringIO(), StringIO()
+            args = command_line_parser().parse_args([
+                "cover",
+                "--coverage_type=path",
+                "--example_output_format=PYTEST",
+                str(target_file_info.absolute_path),
+            ])
+            options = DEFAULT_OPTIONS.overlay(AnalysisOptionSet(
+                per_condition_timeout=60,
+                max_uninteresting_iterations=50,
+            ))
+            retcode = cover(args, options, stdout, stderr)
+        except Exception as exc:
+            traceback.print_exc()
+            return TestGenerationFailure((exc,), FailureReason.UNEXPECTED_ERROR)
+        if retcode == 0:
+            test_body = stdout.getvalue()
+            if test_body:
+                return TestGenerationSuccess(test_body)
+            else:
+                return TestGenerationFailure(("CrossHair did not generate any tests",), FailureReason.NOTHING_GENERATED)
+        else:
+            error_str = stderr.getvalue()
+            return TestGenerationFailure(
+                    (error_str,), FailureReason.UNEXPECTED_ERROR
+                )
